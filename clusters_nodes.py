b8b107794514d8668877ebc8429c15c998de11fd
Damien <damien@dam.io>
2019-07-02 16:24:14 +0200

    display the nodes per cluster with their label

---
+++ clusters_nodes.py
@@ -0,0 +1,60 @@
+import csv, json
+
+## INPUT
+LABELS = json.load(open('example/labels')) # devrait être juste une liste de label mais bon
+EDGES = open("example/X.sp_mat").read() # sparse X
+
+## OUTPUT
+CLUSTERS = open("example/out/0Ccluster(5,4,4)").read()
+
+
+
+clusters = [int(x) for x in 
+    list(csv.reader([CLUSTERS], delimiter=' '))[0]
+    if x is not '']
+
+clusters_nodes = {}
+
+"""
+labels = list(csv.reader([LABELS], delimiter=' '))[0]
+
+def get_label(node):
+	if len(labels) > node:
+		return labels[node]
+	return "no-label-%s" % node
+"""
+labels = LABELS
+def get_label(node):
+	return labels.get(str(node), "no-label-%s" % node)
+
+
+def add_one(node):
+    cluster = clusters[node]
+    if cluster not in clusters_nodes:
+        clusters_nodes[cluster] = {}
+    clust_nodes = clusters_nodes[cluster]
+    if node not in clust_nodes:
+        clust_nodes[node] = 0
+    clust_nodes[node] += 1
+
+for line in csv.reader(EDGES.split('\n'), delimiter=' '):
+    if len(line) == 3:
+        source, target, val = line
+        if val == '0':
+            continue
+        add_one(int(source))
+        add_one(int(target))
+
+def top_10(cluster):
+    nodes = clusters_nodes.get(cluster, {})
+    return [get_label(node) for node, _ in
+                sorted(nodes.items(), key=lambda it: -it[1]) #[:10]
+            ]
+
+top_nodes = [top_10(cluster) for cluster in range(max(clusters) + 1)]
+# return top_nodes # [ [label1_for_cluster_1, label2], [label4_for_cluster_2, label3],.. ]
+
+for i, cluster in enumerate(top_nodes):
+	print("> cluster", i)
+	for node in cluster:
+		print(' -- ', node)
